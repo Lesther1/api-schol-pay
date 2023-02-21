3f174e075c552f2b5fde28cb05d699c50001db6f
Lesther1 <lesthercrespo@gmail.com>
2023-02-21 18:38:47 -0500

    swagger

--- Back/app.js
+++ Back/app.js
@@ -4,11 +4,33 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3000 //puerto declarado en la variable de entorno
 const { dbConnect } = require('./config/mongo') //requiere la conexion
+const path = require("path")
+
+
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerSpec ={ //inicio de swagger
+  definition:{
+    openapi: "3.0.0",
+    info:{
+      title:"school pay api",
+      version: "1.0"
+    },
+    servers:[
+      {
+        url: "https://school-finish.onrender.com/api/1.0/pagosroute"
+      }
+    ]
+  },
+  apis:[`${path.join(__dirname, "./app/routes/pagosroute.js")}`] //rutas
+}
 
 app.use(cors())
 app.use(express.json()) //permite envie de datos
 
-app.use('/api/1.0', require('./app/routes'))
+app.use('/api/1.0', require('./app/routes')) //ruta de la api
+app.use('/api-doc', swaggerUI.serve , swaggerUI.setup(swaggerJsDoc(swaggerSpec))) //ruta de la documentacion
 
 dbConnect()
 app.listen(PORT, ()=>{ //Api escuchando
--- Back/app/routes/pagosroute.js
+++ Back/app/routes/pagosroute.js
@@ -2,10 +2,133 @@ const express = require('express')
 const router = express.Router()
 const {getitem, getitems, createitem, updateitem, deleteitem} = require('../controllers/pagoscontrollers')
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Pago:
+ *       type: object
+ *       properties:
+ *         receipt:
+ *           type: number
+ *           description: The payment receipt number
+ *         date:
+ *           type: string
+ *           description: The payment date (in the format YYYY-MM-DD)
+ *         name:
+ *           type: string
+ *           description: The name of the payer
+ *         concept:
+ *           type: string
+ *           description: The concept of the payment
+ *         value:
+ *           type: number
+ *           description: The payment value
+ */
+
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    sumary: return all pays
+ *    tags: [pay]
+ *    responses:
+ *      200:
+ *        description: all pays!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                 $ref: '#/components/schemas/Pago'
+ */
 router.get('/' , getitems)
+/**
+ * @swagger
+ * /{id}:
+ *  get:
+ *    sumary: return one pays
+ *    tags: [pay]
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: one pays!
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                 $ref: '#/components/schemas/Pago'
+ *      400:
+ *       description: user not found!
+ */
 router.get('/:id', getitem)
+/**
+ * @swagger
+ * /:
+ *  post:
+ *    sumary: create a new pay
+ *    tags: [pay]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *         schema:
+ *          type: object
+ *          $ref: '#/components/schemas/Pago'
+ *    responses:
+ *      200:
+ *        description: new pay is  created!
+ */
 router.post('/', createitem)
+/**
+ * @swagger
+ * /{id}:
+ *  patch:
+ *    sumary: update one pays
+ *    tags: [pay]
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *        description: the user id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *         schema:
+ *          type: object
+ *          $ref: '#/components/schemas/Pago'
+ *    responses:
+ *      200:
+ *        description: pay update!
+ */
 router.patch('/:id', updateitem)
+/**
+ * @swagger
+ * /{id}:
+ *  delete:
+ *    sumary: return one pays
+ *    tags: [pay]
+ *    parameters:
+ *    - in: path
+ *      name: id
+ *      schema:
+ *        type: string
+ *        required: true
+ *        description: the user id
+ *    responses:
+ *      200:
+ *        description: pay update!
+ */
 router.delete('/:id', deleteitem)
 
 module.exports = router
