5fcb6e2d53b48e2a1a6e9329254e06b0a6800b95
Lesther1 <lesthercrespo@gmail.com>
2023-02-22 08:36:13 -0500

    swagger

--- Back/app/routes/index.js
+++ Back/app/routes/index.js
@@ -22,5 +22,25 @@ router.get('*',(req, res) =>{ //cuando busca una ruta que no se consigue
   res.status(404)
   res.send({ error: 'Not Found'})
 })
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
+const path = require("path")
+const swaggerSpec ={ //inicio de swagger
+  definition:{
+    openapi: "3.0.0",
+    info:{
+      title:"school pay api",
+      version: "1.0"
+    },
+    servers:[
+      {
+        url: "https://school-finish.onrender.com/api/1.0/pagosroute"
+      }
+    ]
+  },
+  apis:[`${path.join(__dirname, "./pagosroute.js")}`] //rutas
+}
+router.use('/api-doc', swaggerUI.serve , swaggerUI.setup(swaggerJsDoc(swaggerSpec))) //ruta de la documentacion
+
 
 module.exports = router
